c993f71ec5677935b95aea2b8e5ce3ebde3d5342
cicalecristian <cicalecristian@gmail.com>
2026-04-21 14:51:35 +0200

    Initial commit

---
+++ src/test/java/cristiancicale/G2S3U5/G2S3U5ApplicationTests.java
@@ -0,0 +1,13 @@
+package cristiancicale.G2S3U5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2S3U5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
